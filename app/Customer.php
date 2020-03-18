43ce2979000c874d3e266c2fa1cdd382666bf87c
Bradley Kjeldsen <bradleyk@retailquip.com>
2020-03-18 15:48:16 +1000

    Second Commit

---
+++ app/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    protected $fillable = ['name', 'address', 'postcode', 'city', 'state', 'country', 'postcode', 'phone', 'email'];
+    
+    public function customer_fields()
+    {
+        return $this->hasMany(CustomersField::class);
+    }
+}
---
+++ app/Http/Controllers/InvoicesController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Customer;
+use App\Invoice;
+
+class InvoicesController extends Controller
+{
+    public function create()
+    {
+        return view('invoices.create');
+    }
+    
+    public function store(Request $request)
+    {
+        
+        $customer = Customer::create($request->customer);        
+        $invoice = Invoice::create($request->invoice + ['customer_id' => $customer->id]);
+        for ($i=0; $i < count($request->product); $i++) {
+            if (isset($request->qty[$i])) && isset($request->price[$i]){
+                InvoicesItem::create([
+                    'invoice_id' => $invoice->id,
+                    'name'       => $request->product[$i],
+                    'quantity'   => $request->qty[$i],
+                    'price'      => $request->price[$i],
+                ])
+            }
+        }
+        
+        return 'To be continued';
+    }
+    
+}
